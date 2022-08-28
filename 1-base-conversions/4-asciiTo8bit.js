be1f5062c8c4d435c69ba3b514dba6ff8c1f53e5
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-28 11:51:11 -0400

    Implement asciiTo8bit()

--- 1-base-conversions/4-asciiTo8bit.js
+++ 1-base-conversions/4-asciiTo8bit.js
@@ -6,8 +6,38 @@ const addZeros = require("../utils/addZeros");
 
 /******************************************************************************/
 
+const convertToBase2 = element => {
+  let binary = Array(Math.floor(Math.log2(element)) + 1).fill(0);
+  for (let i = 0; i < binary.length; i++) {
+    let value = Math.pow(2, binary.length - 1 - i);
+    if (element >= value) {
+      element -= value;
+      binary[i] = 1;
+    }
+  }
+  return binary.join("");
+};
+
 const asciiTo8bit = str => {
-  // Your code here
+  let binary = "";
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  const punctuation = " !\"#$%&'()*+,-./";
+  for (let i = 0; i < str.length; i++) {
+    if (alpha.includes(str[i].toLowerCase())) {
+      const index = alpha.indexOf(str[i].toLowerCase());
+      binary += addZeros(
+        convertToBase2(
+          str[i] === str[i].toUpperCase() ? 65 + index : 97 + index
+        ),
+        8
+      );
+    } else if (punctuation.includes(str[i])) {
+      binary += addZeros(convertToBase2(32 + punctuation.indexOf(str[i])), 8);
+    } else if (!isNaN(Number(str[i]))) {
+      binary += addZeros(convertToBase2(48 + Number(str[i])), 8);
+    }
+  }
+  return binary;
 };
 
 /******************************************************************************/
